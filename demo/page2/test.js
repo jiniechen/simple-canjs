5da07697b7bc59377d267cbb39b7c16697f50ae3
Xueyy2017 <444740823@qq.com>
2017-02-22 15:38:04 +0800

    scrollview

--- demo/page2/test.js
+++ demo/page2/test.js
@@ -29,86 +29,77 @@ Page({
 			]
 		});
 	},
+
 	//dialog:true,
 	title:"这是我的Dialog标题",
 	//closeBtn:false,
 	onShow:function(page){
+		
 		//page.doIt();
 	},
 	onHide:function(page){
 		//alert("hide:"+page.name);
+		
 	},
 	onLoad:function(page){
 		//alert("load:"+page.name);
+		exports.Mask.show();
 	},
 	doIt:function(){
+		
 		//alert("call function doIt()");
 	},
 	onScrollUp:function(el){
+
 		var detail =[
-				{id:"001",name:"初始化信息",
+				{id:"001",name:"上一页信息",
 					time:"2017"
 				},
-				{id:"002",name:"初始化信息",
+				{id:"002",name:"上一页信息",
 					time:"2017"
 				},
-				{id:"003",name:"初始化信息",
+				{id:"003",name:"上一页信息",
 					time:"2017"
 				},
-				{id:"004",name:"初始化信息",
+				{id:"004",name:"上一页信息",
 					time:"2017"
 				},
-				{id:"005",name:"初始化信息",
+				{id:"005",name:"上一页信息",
 					time:"2017"
 				}
 				
-			] 
+			] ;
+		var data  = {
+			_data : detail,
+			count : 3
+		};
+		return data;
 
-		var _data =el.data;
-		_data.splice(0,_data.length);
-		_data.attr(detail);
-		el.lastPage= false;
 	},
 	onScrollDown:function(el){
-		//debugger;
 		var detail =[
-				{id:"001",name:"新增信息1",
+				{id:"001",name:"下一页信息",
 					time:"2017"
 				},
-				{id:"002",name:"新增信息1",
+				{id:"002",name:"下一页信息",
 					time:"2017"
 				},
-				{id:"003",name:"新增信息1",
+				{id:"003",name:"下一页信息",
 					time:"2017"
 				},
-				{id:"004",name:"新增信息1",
+				{id:"004",name:"下一页信息",
 					time:"2017"
 				},
-				{id:"005",name:"新增信息1",
-					time:"2017"
-				},
-				{id:"006",name:"新增信息1",
-					time:"2017"
-				},
-				{id:"007",name:"新增信息1",
-					time:"2017"
-				},
-				{id:"008",name:"新增信息1",
-					time:"2017"
-				},
-				{id:"009",name:"新增信息1",
-					time:"2017"
-				},
-				{id:"010",name:"新增信息1",	
+				{id:"005",name:"下一页信息",
 					time:"2017"
 				}
 				
-			] 
-
-		var _data =el.data;
-		_data.splice(0,_data.length);
-		_data.attr(detail);
-		el.lastPage= true;
+			] ;
+		var data  = {
+			_data : detail,
+			count : 3
+		};
+		return data;
 	},
 	onScrollData:function(el) {
 		var detail =[
@@ -128,11 +119,12 @@ Page({
 					time:"2017"
 				}
 				
-			] 
-		el.viewModel().lastPage = false;
-		var _data =el.viewModel().data;
-		_data.splice(0,_data.length);
-		_data.attr(detail);
+			] ;
+		var data  = {
+			_data : detail,
+			count : 3
+		};
+		return data;
 
 	}
 }); 
--- myFramework/ui/ScrollView.js
+++ myFramework/ui/ScrollView.js
@@ -1,81 +1,8 @@
-requirejs([ "text!myFramework/ui/ScrollView.stache","myFramework/ui/WidgetFactory"],function(tpl,widgetFactory){
-	widgetFactory.widget("scrollview",tpl)
-	.config(function(config){
-		config.hasError=false;
-		config.hasAlign=false;
-		config.hasLabel=false;
-		config.extendVM=function(vm,attrs,parentScope,el){
-			vm.title=undefined;
-			vm.pageNumber = 1;//第几页
-			vm.firstPage = true;//是否为第一页
-			vm.lastPage = false;//是否为最后一页
-		}
-	})
-	.events(function(events){
-
-		events["#prePage click"]=function(el){
-			var _pageNumber = this.viewModel.pageNumber;
-			var _viewModel = this.viewModel;
-			if(_pageNumber != 1){
-				var page = _viewModel.page
-			    var _name  =_viewModel.id||_viewModel.name||"";
-			    var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
-			    if(page["on"+funcName+"Up"]){
-			    	exports.Mask.show();
-			    	var timer = setTimeout(function(){
-			    		exports.Mask.hide();
-			    		page["on"+funcName+"Up"](_viewModel);
-				    	_pageNumber--;
-						_viewModel.attr("pageNumber",_pageNumber);
-						if(_pageNumber == 1){
-							_viewModel.attr("firstPage",true);
-						};
-			    	},500);
-			    };
-			}
-		};
-		events["#nextPage click"]=function(el){
-			var _lastPage = this.viewModel.lastPage;
-			var _viewModel = this.viewModel;
-			if(!_lastPage){
-				var _pageNumber = _viewModel.pageNumber;
-				var page = _viewModel.page
-			    var _name  =_viewModel.id||_viewModel.name||"";
-			    var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
-			    if(page["on"+funcName+"Down"]){
-			    	exports.Mask.show();
-			    	var timer = setTimeout(function(){
-			    		exports.Mask.hide();
-			    		page["on"+funcName+"Down"](_viewModel);
-				    	_pageNumber++;
-						_viewModel.attr("pageNumber",_pageNumber);
-						_lastPage = _viewModel.lastPage;
-						_viewModel.attr("firstPage",false);
-			    	},500);
-			    }
-			}
-		}
-		events.inserted = function(el,ev){
-			var page = el.viewModel().page;
-			var _name = el.viewModel().id||el.viewModel().name||"";
-			var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
-			if(page["on"+funcName+"Data"]){
-				page["on"+funcName+"Data"](el);
-			};	
-			var _pageNumber =  el.viewModel().pageNumber;
-		};
-	})
-	.build()
-	.plugin(function(_widget){
-	})
-});
-
-	/*can.Component.extend({
+requirejs([ "text!myFramework/ui/ScrollView.stache"],function(tpl){
+can.Component.extend({
 		tag : "scrollview",
-		helpers:stacheHelpers,
 		template : can.stache(tpl),
 		viewModel : function(attrs,parentScope,el){
-			
 			//获取page对象的viewModel,组合组件从上层组件获取root,顶层组件的parentScope为root
 			var _root=parentScope.attr("root")==undefined?parentScope:parentScope.attr("root");
 			//获取页面对象
@@ -89,9 +16,9 @@ requirejs([ "text!myFramework/ui/ScrollView.stache","myFramework/ui/WidgetFactor
 				id:el.getAttribute("id"),
 				contextName:_contextName,
 				name:undefined,
-				title:undefined,
-				validate:"true",
-				height:el.getAttribute("height"),
+				pageNumber:1,
+				preClass:"gray",
+				nextClass:"gray",
 				page:_page,
 				data:_data,
 				root:_root,
@@ -99,51 +26,68 @@ requirejs([ "text!myFramework/ui/ScrollView.stache","myFramework/ui/WidgetFactor
 			};
 		},
 		events:{
-			"inserted" :function(el,ev){
-				
-				var section = el.find("section").eq(0),
-					div = section.find("div").eq(0);
-
-				var _viewModel = getCurrentPage();
-
-				var wrapH = el.viewModel().height,//容器高度
-					documentH = div.height(),//文档高度
-					data = el.viewModel().context,
-					data = data.substring(0,1).toUpperCase()+data.substring(1,data.length);
-
-				
-				if(_viewModel["on"+data+"Up"])
-					var up = _viewModel["on"+data+"Up"];
-				else 
-					up = undefined;
-				if(_viewModel["on"+data+"Down"])
-					var down = _viewModel["on"+data+"Down"];
-				else
-					down = undefined;
-				section.listenScroll({
-					listenScroll:function(isInScroll, scrollDirection, scrollTop){
-
-						if(scrollDirection == "down"){
-							
-							if(scrollTop == documentH-wrapH){
-								
-								var timer = setTimeout(function(){
-									if(_viewModel["on"+data+"Up"])
-										_viewModel["on"+data+"Up"]();
-								},100);
-								
-							}
-						}else if(scrollDirection == "up"){
-							if(scrollTop == 0){
-								var timer = setTimeout(function(){
-									if(_viewModel["on"+data+"Down"])
-										_viewModel["on"+data+"Down"]();
-								},100);
-							}
-						}
-					}
-				})
+			"inserted":function(el,ev){
+				var _viewModel = this.viewModel;
+				var _page  = _viewModel.page;
+				var pageNumber = _viewModel.pageNumber;
+				var _name  =_viewModel.id||_viewModel.name||"";
+			    var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
+			    var count ;
+			    var _data ;
+			    if(_page["on"+funcName+"Data"]){
+			    	var _ondata = _page["on"+funcName+"Data"]();
+			    	can.each(_ondata,function(val,key){
+			    		key == "count" ? count = val :_data = val;
+			    	});
+			    	_viewModel.attr("nextClass",count > 1 ? "primary" : "gray");
+			    	_viewModel.attr("data",_data);
+			    };
+			},
+			"#prePage click" :function(){
+				var _viewModel = this.viewModel;
+				var _page  = _viewModel.page;
+				var pageNumber = _viewModel.pageNumber;
+				var _name  =_viewModel.id||_viewModel.name||"";
+			    var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
+			    var count ;
+			    var _data ; 
+			    if(_page["on"+funcName+"Up"]){
+			    	pageNumber--;
+			    	var preData = _page["on"+funcName+"Up"]();
+		    		can.each(preData,function(val,key){
+		    			key == "count" ? count = val :_data = val;
+		    		});
+			    	if(pageNumber > 1 || pageNumber == 1){
+			    		_viewModel.attr("pageNumber",pageNumber);
+			    		_viewModel.attr("data",_data);
+			    		_viewModel.attr("preClass",pageNumber == 1 ? "gray" : "primary");
+			    		_viewModel.attr("nextClass",pageNumber == count ? "gray" : "primary");
+			    		exports.Mask.hide();
+				    }
+			    };
+			},
+			"#nextPage click" :function(){
+				var _viewModel = this.viewModel;
+				var _page  = _viewModel.page;
+				var pageNumber = _viewModel.pageNumber;
+				var _name  =_viewModel.id||_viewModel.name||"";
+			    var funcName = _name.substring(0,1).toUpperCase()+_name.substring(1,_name.length);
+			    var count ;
+			    var _data ; 
+			    if(_page["on"+funcName+"Down"]){
+		    		pageNumber++;
+		    		var nextData = _page["on"+funcName+"Down"]();
+		    		can.each(nextData,function(val,key){
+		    			key == "count" ? count = val :_data = val;
+		    		});
+		    		if(pageNumber < count || pageNumber == count ){
+		    			_viewModel.attr("pageNumber",pageNumber);
+		    			_viewModel.attr("data",_data);
+		    			_viewModel.attr("nextClass",pageNumber == count ? "gray" : "primary");
+		    			_viewModel.attr("preClass","primary");
+		    		}
+			    };
 			}
 		}
 	});
-});*/
+});
